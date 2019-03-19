351db5097c59d6a61e74529a4cbf39b981c20da3
gowthamnagaraj94 <gowthamnagaraj1234@gmail.com>
2019-03-19 17:18:07 +0530

    task1

---
+++ task1/src/task1/T1.java
@@ -0,0 +1,15 @@
+package task1;
+
+public class T1 {
+	public static void main(String[] args) {
+		int a=10;
+	if(a%2==0) {
+		System.out.println("even");
+	}
+	else {
+		System.out.println("odd");
+	}
+	
+
+}
+}
